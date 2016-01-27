f7efc07e88c8534bfc0404db08c299a53a661761
Chao-Yu Cheng <chaoyucheng001@gmail.com>
2016-01-27 10:16:59 -0500

    Signed-off-by: Chao-Yu Cheng <chaoyucheng001@gmail.com>

--- server.R
+++ server.R
@@ -8,21 +8,26 @@ shinyServer(
   function(input, output) {    
     output$newPlot <- renderPlot({  
       mtcars$am <- factor(mtcars$am,levels=c(0,1), labels=c("Automatic","Manual"))
-      if (input$predictType == "cyl") {
+      if (input$predictType == "am") {
+        p <- qplot(am, mpg, data=mtcars, geom=c("boxplot"), fill=am,
+                   main="MPG by Transmission", xlab="Transmission", ylab="MPG")
+        print(p)
+      } else if (input$predictType == "cyl") {
         p <- qplot(cyl, mpg, data=mtcars, geom=c("point", "smooth"),
                  color=am, shape=am,
                  main="MPG by number of cylinders", xlab="Cylinder", ylab="MPG")
+        print(p)
       } else if (input$predictType == "wt") {
         p <- qplot(wt, mpg, data=mtcars, geom=c("point", "smooth"),
                    color=am, shape=am,
                    main="MPG by number of Weight", xlab="Weight", ylab="MPG")
-      } else {
+        print(p)
+      } else if (input$predictType == "hp") {
         p <- qplot(hp, mpg, data=mtcars, geom=c("point", "smooth"),
                    color=am, shape=am,
                    main="MPG by number of Horsepower", xlab="Horsepower", ylab="MPG")
+        print(p)
       }
-      
-      print(p)
     })
   }
 )
--- ui.R
+++ ui.R
@@ -1,20 +1,23 @@
 library(shiny)
 library(manipulate)
 shinyUI(pageWithSidebar(  
-  headerPanel("MPG versus cylinder, weight or horsepower"),  
+  headerPanel("Exploratory ploting on mtcars dataset"),  
   sidebarPanel( 
-    radioButtons("predictType", "Prediction factor:",
-               c("Cylinder" = "cyl", "Weight" = "wt", "Horsepower" = "hp")),
+    radioButtons("predictType", "Please choose an item you are interested:",
+               c("MPG v.s. Transmission" = "am",
+                 "MPG v.s. Cylinder" = "cyl",
+                 "MPG v.s. Weight" = "wt",
+                 "MPG v.s. Horsepower" = "hp")),
     br(),
     p(strong(em("Github repository:",
-        a("Developing Data Products - Peer Assessment Project; Shiny App",
+        a("Developing Data Products Course Project: A Shiny Appplication",
         href="https://github.com/chaoyucheng/Shiny-Application.git")))),
     p(strong(em("Documentation: In the mtcars dataset, we are specifically
       interested in some factors which may have close relation with the MPG.
       We construct this simple application to demonstrate the fact.
       Please select a radio button on Cylnder, Weight or Horsepower.
       In the main panel, the application will display the plot on MPG against
-      the selected factor. The plot will seperated Automate/Manual
+      the selected factor. The plot will seperated Automatic/Manual
       transmission by different colors and shapes")))
   ),
   mainPanel(    
